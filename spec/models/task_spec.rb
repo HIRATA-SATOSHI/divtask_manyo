669db0650d69d4b56b6d88a5f7eda8ffa94b7227
satoshi.hirata <s.hirata0507@gmail.com>
2020-08-16 10:55:22 +0900

    model Spec修正　#1

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,23 +3,24 @@ describe 'タスクモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト')
+        task = Task.new(name: '', detail: '失敗テスト')
         expect(task).not_to be_valid
       end
   end
-　　
-　　context 'タスクの詳細が空の場合' do
+  
+  context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
         # ここに内容を記載する
-        task = Task.new(title: '詳細がからのテスト', content: '')
+        task = Task.new(name: '詳細がからのテスト', detail: '')
         expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
         # ここに内容を記載する
-        task = Task.new(title: 'hogehoge', content: 'gehogeho')
+        task = Task.new(name: 'hogehoge', detail: 'gehogeho')
         expect(task).to be_valid
       end
     end
+  end
 end
