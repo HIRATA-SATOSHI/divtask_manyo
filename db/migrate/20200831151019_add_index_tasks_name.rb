604dbb3c2a63fb1780ac22198f6161eb48bd5c6a
satoshi.hirata <s.hirata0507@gmail.com>
2020-09-01 00:12:54 +0900

    検索Index修正追加 SystemSpecエラー途中 #3

--- db/migrate/20200831151019_add_index_tasks_name.rb
+++ db/migrate/20200831151019_add_index_tasks_name.rb
@@ -1,5 +1,5 @@
 class AddIndexTasksName < ActiveRecord::Migration[5.2]
   def change
-    add_index :tasks, :status
+    add_index :tasks, :name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_150510) do
+ActiveRecord::Schema.define(version: 2020_08_31_151019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_150510) do
     t.date "deadline", null: false
     t.integer "status"
     t.integer "priority"
+    t.index ["name"], name: "index_tasks_on_name"
   end
 
 end
