10fb38e00947fa1ee7f7e239505f69173822799b
satoshi.hirata <s.hirata0507@gmail.com>
2020-09-09 00:14:30 +0900

    Validates 修正 #3

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
     validates :name, presence: true, length: { maximum: 140 }
     validates :detail, presence: true,length: { maximum: 500 }
     validates :deadline, presence: true
+    validates :status, presence: true
+    validates :priority, presence: true
     scope :name_search, -> (params) {where('(name LIKE ?)',"#{params[:task][:name]}")}
     scope :status_search, -> (params) {where(status: params[:task][:status])}
     enum status:{ not_yet: 0, in_progress: 1, completed: 2 }
