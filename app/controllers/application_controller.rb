15a265282f6eff068ee6ec8f37c131f3df43f47b
satoshi.hirata <s.hirata0507@gmail.com>
2020-09-14 03:01:56 +0900

    Loginしていない人の制御 実装中　#4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@ class ApplicationController < ActionController::Base
 
     def authenticate_user
         # 現在ログイン中のユーザが存在しない場合、ログインページにリダイレクトさせる。
-        if @current_user == nil
-          flash[:notice] = t('notice.ログインが必要です！')
-          redirect_to new_session_path
-        end
+      if @current_user == nil
+        flash[:notice] = t('notice.ログインが必要です！')
+        redirect_to new_session_path
       end
+    end
+
+  private
+    def logged_in
+      unless logged_in?
+        redirect_to new_session_path
+      end
+    end
 end
