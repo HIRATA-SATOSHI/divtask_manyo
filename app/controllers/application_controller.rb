4dbb0c51c6cf81f6aace98a8f727da77d948044d
satoshi.hirata <s.hirata0507@gmail.com>
2020-09-14 22:30:01 +0900

    ログインしないとTASK一覧にいけないwo実装 　#4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :login_required
     protect_from_forgery with: :exception
     include SessionsHelper
-
+    
     def authenticate_user
         # 現在ログイン中のユーザが存在しない場合、ログインページにリダイレクトさせる。
       if @current_user == nil
@@ -11,9 +12,7 @@ class ApplicationController < ActionController::Base
     end
 
   private
-    def logged_in
-      unless logged_in?
-        redirect_to new_session_path
-      end
-    end
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
 end
