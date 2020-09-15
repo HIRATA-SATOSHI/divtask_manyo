9516f3ed5f7e3ab2b180fe6e61ad5947910138c1
satoshi.hirata <s.hirata0507@gmail.com>
2020-09-16 00:22:42 +0900

    session画面からSignupへ行かない AugmnentError #4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
-    before_action :login_required
+    skip_before_action :login_required, only: [:new, :create]
     protect_from_forgery with: :exception
     include SessionsHelper
     
-    def authenticate_user
-        # 現在ログイン中のユーザが存在しない場合、ログインページにリダイレクトさせる。
-      if @current_user == nil
-        flash[:notice] = t('notice.ログインが必要です！')
-        redirect_to new_session_path
-      end
-    end
+    # def authenticate_user
+    #     # 現在ログイン中のユーザが存在しない場合、ログインページにリダイレクトさせる。
+    #   if @current_user == nil
+    #     flash[:notice] = t('notice.ログインが必要です！')
+    #     redirect_to new_session_path
+    #   end
+    # end
 
   private
   def login_required
