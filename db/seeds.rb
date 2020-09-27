4d7eb0f8c525f72125d13dd0070e13b7d0c223bd
satoshi.hirata <s.hirata0507@gmail.com>
2020-09-27 20:07:07 +0900

    seed.rb 管理者ユーザ記述 #4

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,9 @@
                  password_confirmation: password,
                  )
   end
+
+  User.create!(username:  "管理者",
+             email: "admin@example.jp",
+             password:  "11111111",
+             password_confirmation: "11111111",
+             admin: true)
