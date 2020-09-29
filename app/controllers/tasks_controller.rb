ec386a9b3ef7a316f1d2af9ebca7c50553e6a330
satoshi.hirata <s.hirata0507@gmail.com>
2020-09-30 00:08:37 +0900

    taskの部分のeditとdelete修正ずみ #4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :logged_in_user, only:[:edit, :update, :destroy]
+  before_action :current_user, only:[:edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   # GET /tasks
