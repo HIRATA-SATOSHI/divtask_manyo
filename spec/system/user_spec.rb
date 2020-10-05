49bf290a381e952a8e3e74050b55f22258fabd3e
satoshi.hirata <s.hirata0507@gmail.com>
2020-10-06 00:49:23 +0900

    admin systemspec test compleate #4

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -64,6 +64,76 @@ RSpec.describe 'ユーザ登録・ログイン・ログアウト機能・管理�
     end     
   end
 
+  describe "管理画面のテスト" do
+    context "管理ユーザ作成" do
+      it "管理者は管理画面にアクセスできること" do
+        FactoryBot.create(:second_user)
+        visit new_session_path
+        fill_in "session_email", with: "test_user_02@test.com​"
+        fill_in "session_password", with: "87654321"
+        click_on "Log in"
+        visit admin_users_path
+        expect(page).to have_content "ユーザー登録一覧"       
+      end
+    end
+
+    context "一般ユーザーがログインしている場合" do
+      it "一般ユーザーは管理画面にはアクセスできない" do
+        FactoryBot.create(:user)
+        visit new_session_path
+        fill_in "session_email", with: "test_user_01@test.com​"
+        fill_in "session_password", with: "12345678"
+        click_on "Log in"
+        visit admin_users_path
+        expect(page).to have_content "あなたは管理者ではありません" 
+      end         
+    end
+
+    context "管理者でログインしている場合" do
+      before do
+        FactoryBot.create(:second_user)
+        visit new_session_path
+        fill_in "session_email", with: "test_user_02@test.com​"
+        fill_in "session_password", with: "87654321"
+        click_on "Log in"
+        visit admin_users_path      
+      end
+
+      it "管理者はユーザ新規登録ができる" do
+        click_link "新規ユーザー作成"
+        fill_in "user_name", with: "Tomioka Giyu"
+        fill_in "user_email", with: "g.tomooka@test.com"
+        fill_in "user_password", with: "23456789"
+        fill_in "user_password_confirmation", with: "23456789"
+        click_on "登録する"
+        expect(page).to have_content "Tomioka Giyu"       
+      end
+
+      it "管理者はユーザの詳細画面へ行ける" do
+        @user = FactoryBot.create(:user)
+        visit admin_user_path(id: @user.id)
+        expect(page).to have_content "test_user_01" 
+      end
+      
+      it "管理者ユーザーの編集画面からユーザーの編集ができる" do
+        @user = FactoryBot.create(:user)
+        visit edit_admin_user_path(id: @user.id)
+        fill_in 'user_name', with: 'test_user_0100'
+        fill_in 'user_email', with: 'test_user_0100@test.com'
+        fill_in 'user_password', with: '12345678'
+        fill_in 'user_password_confirmation', with: '12345678'
+        click_button '更新する'
+        expect(page).to have_content "test_user_0100"
+      end
+      
+      it "管理者はユーザーを削除できる" do
+        @user = FactoryBot.create(:user)
+        visit admin_users_path
+        click_link "Delete", match: :first
+      end
+    end
+  end
+
 end
 
 
