ca9804e40e3dd01fa14929021cf630b1cfb32f23
satoshi.hirata <s.hirata0507@gmail.com>
2020-10-07 02:31:47 +0900

    task_spec 修正中 ソート　#4

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     # 作成するテストデータの名前を「task」とします
     # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
     factory :task do
-      name { 'Factoryで作ったデフォルトのタイトル１' }
-      detail { 'Factoryで作ったデフォルトのコンテント１' }
+      name { 'Factoryで作ったデフォルトのタイトル1' }
+      detail { 'Factoryで作ったデフォルトのコンテント1' }
       deadline { DateTime.now }
       status {'completed'}
       priority {'high'}
@@ -13,8 +13,8 @@ FactoryBot.define do
     # 作成するテストデータの名前を「second_task」とします
     # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
     factory :second_task, class: Task do
-      name { 'Factoryで作ったデフォルトのタイトル２' }
-      detail { 'Factoryで作ったデフォルトのコンテント２' }
+      name { 'Factoryで作ったデフォルトのタイトル2' }
+      detail { 'Factoryで作ったデフォルトのコンテント2' }
       deadline { DateTime.tomorrow }
       status {'not_yet'}
       priority {'middle'}
