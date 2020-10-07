95401f6edc8df4744d5d1c594d659a73c211d0ab
satoshi.hirata <s.hirata0507@gmail.com>
2020-10-08 01:22:35 +0900

    フィードバックFooter修正 #4

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do |n|
+15.times do |n|
     name = Faker::Games::Pokemon.name
     email = Faker::Internet.email
     password = "password"
