e94b2f3e59ec46dde450b11e3ba2cb472d127189
satoshi.hirata <s.hirata0507@gmail.com>
2020-10-07 23:41:49 +0900

    フィードバックFaker修正対応 #4

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TaskManyo
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
+    # config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators do |g|
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@
                  )
   end
 
-  User.create!(username:  "管理者",
+  User.create!(name:  "管理者",
              email: "admin@example.jp",
              password:  "11111111",
              password_confirmation: "11111111",
