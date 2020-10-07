00d79742c4c21faee6da1550c44987d6ec0901b4
satoshi.hirata <s.hirata0507@gmail.com>
2020-10-07 16:39:14 +0900

    フィードアックbefore_validationの削除 #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_many :tasks, dependent: :destroy
     has_secure_password
     before_save { email.downcase! }
-    before_validation { email.downcase! }
+    # before_validation { email.downcase! }
     validates :name,  presence: true, length: { maximum: 30 }
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
